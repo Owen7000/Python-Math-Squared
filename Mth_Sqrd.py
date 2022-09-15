a91c1178992bf4b2d865e1d8b585ecb9e93eab61
Owen <77976026+Owen7000@users.noreply.github.com>
2022-09-15 13:11:05 +0100

    commit

---
+++ Mth_Sqrd.py
@@ -0,0 +1,33 @@
+def pi():
+    """Returns the first 100 digits of pi"""
+    pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
+    return pi
+
+def e():
+    """Returns the first 100 digits of Eulers number"""
+    e =2.71828182845904523536028747135266249775724709369995957496696762772407663035354759457138217852516642742
+    return eua
+
+def tau():
+    """Returns the first 100 digits of tau"""
+    tau = 6.2831853071795864769252867665590057683943387987502116419498891846156328125724179972560696506842341359
+    return tau
+
+def sqrt(x):
+    """Returns the square root of value 'X'"""
+    return x**(1/2)
+
+def sqr(x):
+    """Returns the square of value 'X'"""
+    return x*x
+
+# def DF(TL, BL):
+#     L = len(TL)
+#     if len(BL) > L:
+#         L = len(BL)
+#     lAr = ["-"]
+#     for x in  L:
+#         LAr.append("-")
+#     lAr.append("-")
+
+#     print(TL, "\n" +  LAr + "\n" + BL)
---
+++ Mth_Sqrd_Eng.py
@@ -0,0 +1,17 @@
+def Ep(m, g, h):
+    return m*g*h
+
+def Ek(m, v):
+    hm = m/2
+    vs = v*v
+    return hm*vs
+
+def Eh(c, m, ΔT):
+    return c*m*ΔT
+
+def Ee(V, I, t):
+    return V*I*t
+
+def Ew(F, d):
+    return F*d
+
